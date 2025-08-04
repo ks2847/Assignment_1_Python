e2b651e97ea574f17f029fd0c5250231a84b9006
ks2847 <ksanjeev5898@gmail.com>
2025-08-04 18:38:13 +0530

    Add files via upload

---
+++ Assignment _1_Python.py
@@ -0,0 +1,21 @@
+# Task_1
+'''
+num_1 = input('Enter the first number: ')
+num_2 = input('Enter the 2nd number: ')
+num_1 = float(num_1)
+num_2 = float(num_2)
+sum = num_1 + num_2
+sub = num_1 - num_2
+mul = num_1 * num_2
+div = num_1 / num_2
+print('Addition: ',sum)
+print('Subtraction: ',sub)
+print('Multiplication: ',mul)
+print('Division: ',div)
+'''
+
+# Task 2
+f_name = input('Enter your First name: ')
+l_name = input('Enter your last name: ')
+l_name = l_name + "!"
+print('Hello,',f_name,l_name,'Welcome to the Python program.')
